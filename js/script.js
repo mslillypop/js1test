c83e82be3cd0d87fd4c1d66141f40033eefe02ac
Lilly <119416005+mslillypop@users.noreply.github.com>
2023-05-05 23:58:40 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,5 +6,3 @@ const baseUrl = "https://api.noroff.dev/api/v1/rainy-days/";
 import { fetchProduct} from "./products.js";
 fetchProduct()
 
-import { fetchProductDetail } from "./detail.js";
-fetchProductDetail()
